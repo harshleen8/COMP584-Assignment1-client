836e179385a4fe3005e0cf2516f0d61bceb6099c
Harshleen <harshleensadhnani8@gmail.com>
2023-03-06 20:02:13 -0800

    format changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PropertyListComponent } from './property/property-list/property-list.component';
-import { PropertyCardComponent } from './property/property-card/property-card.component';
+import { PropertyListComponent } from './property-list/property-list.component';
+import { PropertyCardComponent } from './property-card/property-card.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { HomeComponent } from './home/home.component';
-import { MatButtonModule } from "@angular/material/button";
-import { MatIconModule } from "@angular/material/icon";
-import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
