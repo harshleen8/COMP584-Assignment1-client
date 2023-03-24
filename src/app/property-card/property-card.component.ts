c1e89d87bf326628029d3c1fabe196c9cb687933
Harshleen <harshleensadhnani8@gmail.com>
2023-03-24 01:56:08 -0700

    property list changes

--- src/app/property-card/property-card.component.ts
+++ src/app/property-card/property-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-property-card',
@@ -7,11 +7,6 @@ import { Component } from '@angular/core';
 })
 export class PropertyCardComponent {
 
-  Property: any = {
-    "Id":1,
-    "Name":"Zelzah Court",
-    "Type":"House",
-    "Price": 12000
-  }
+  @Input() property : any
 
 }
--- src/app/property-list/property-list.component.ts
+++ src/app/property-list/property-list.component.ts
@@ -7,4 +7,43 @@ import { Component } from '@angular/core';
 })
 export class PropertyListComponent {
 
+  properties: Array<any> = [
+    {
+    "Id":1,
+    "Name":"Zelzah Court",
+    "Type":"House",
+    "Price": 12000
+    },
+    {
+      "Id":2,
+      "Name":"Maravilla",
+      "Type":"House",
+      "Price": 19000
+    },
+    {
+      "Id":3,
+      "Name":"Candlewood",
+      "Type":"House",
+      "Price": 71000
+    },
+    {
+      "Id":4,
+      "Name":"Meridian Pointee",
+      "Type":"House",
+      "Price": 21000
+    },
+    {
+      "Id":5,
+      "Name":"Meridian Place",
+      "Type":"House",
+      "Price": 10000
+    },
+    {
+      "Id":6,
+      "Name":"Parc ridge",
+      "Type":"House",
+      "Price": 18000
+    }
+]
+
 }
